b710018f518567afcb463e1924762ea4c08c302d
Shin Hiroe <hiroe.orz@gmail.com>
2014-05-18 11:34:31 +0900

    modify test

--- test/mqtt_client_test.rb
+++ test/mqtt_client_test.rb
@@ -1,6 +1,7 @@
 MQTT_TOPIC_1= "/mqtttest/test1"
 MQTT_TOPIC_2 = "/mqtttest/test2"
-MQTT_PAYLOAD = "hello"
+MQTT_PAYLOAD_1 = "hello"
+MQTT_PAYLOAD_2 = "world"
 
 assert("MQTTClient.connect") do
   subscribe_count = 0
@@ -12,21 +13,21 @@ assert("MQTTClient.connect") do
 
     c.on_connect   = -> {
       c.subscribe(MQTT_TOPIC_1, 0)
-      c.publish(MQTT_TOPIC_1, "hello")
-      c.publish(MQTT_TOPIC_2, "world")
+      c.publish(MQTT_TOPIC_2, MQTT_PAYLOAD_2)
       c.publish(MQTT_TOPIC_2, "this is mruby-mqtt libraries")
+      c.publish(MQTT_TOPIC_1, MQTT_PAYLOAD_1) # subscribed
       c.publish(MQTT_TOPIC_2, "neko")
       c.publish(MQTT_TOPIC_2, "inu")
     }
 
     c.on_message = -> (message) {
       assert_equal MQTT_TOPIC_1, message.topic
-      assert_equal MQTT_PAYLOAD, message.payload
+      assert_equal MQTT_PAYLOAD_1, message.payload
     }
   end
 
 
-  Sleep.sleep 5
+  Sleep.sleep 2
   assert_equal 1, subscribe_count
   assert_equal 5, publish_count
 end
